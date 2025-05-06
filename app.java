822b2d0f3856ce8166535b0d341a9e17b6f12c5a
Hilda Styles <developer1eneteam@outlook.com>
2025-05-06 18:33:20 +0000

    commit done for git revert

--- app.java
+++ app.java
@@ -1,5 +1,5 @@
-echo "Please enter your usernames"
+echo "Please enter your username"
             read username
-            echo "Please user enter your password"
+            echo "Please users  enter your password"
             read -s password
             echo "Welcome to the  Paypal application"
